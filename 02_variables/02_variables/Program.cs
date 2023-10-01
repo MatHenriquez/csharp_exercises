1cd62f665300cc24c97e7ff2e01e6cfadfa486b3
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-01 16:51:18 -0300

    literals

---
+++ 02_variables/02_variables/Program.cs
@@ -0,0 +1,12 @@
+﻿// Literales
+
+Console.WriteLine('b'); // Imprime el literal de tipo character b, que va entre comillas simples.
+// Console.WriteLine('Hello, World!'); // Da error porque se intenta guardar un string en comillas simples.
+Console.WriteLine(123); // Imprime el literal de tipo integer 123.
+Console.WriteLine(0.25F); // Imprime el literal de tipo float 0.25. F (o f) se denomina 'sufijo literal'.
+Console.WriteLine(2.625); // Imprime el literal de tipo double 2.625. Al no tener sufijo literal, es double por defecto.
+Console.WriteLine(12.39816m); // Imprime el literal de tipo decimal 12.39816. m (o M) se denomina 'sufijo literal'.
+
+// Literales de tipo booleano
+Console.WriteLine(true);
+Console.WriteLine(false);
