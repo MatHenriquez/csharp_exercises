446ba8bc2c9aa2cc5aede281160debdc7325bc0d
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-02 20:38:10 -0300

    escape character sequence

---
+++ 03_string_format/03_string_format/Program.cs
@@ -0,0 +1,5 @@
+﻿// Secuencia de caracteres de escape.
+Console.WriteLine("Hello\nWorld!"); // La secuencia \n agregará una nueva línea.
+Console.WriteLine("Hello\tWorld!"); // La secuencia \t agregará una tabulación.
+Console.WriteLine("Hello \"World\"!"); // La secuencia \" agregará comillas dobles.
+Console.WriteLine("c:\\source\\repos"); // La secuencia \\ agregará una barra invertida.
