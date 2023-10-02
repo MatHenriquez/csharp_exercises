8766e4ed3f2c300a604a728c255891c8e4397376
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-02 20:48:18 -0300

    Unicode escape characters: format

--- 03_string_format/03_string_format/Program.cs
+++ 03_string_format/03_string_format/Program.cs
@@ -16,3 +16,12 @@ Console.WriteLine(@"    c:\source\repos
 Console.WriteLine(@"c:\invoices");
 
 // Caracteres de escape Unicode
+
+// Kon'nichiwa World
+Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!"); // La secuencia u seguido de cuatro caractes que representes un caracter Unicode (UTF-16), genera un caracter codificado.
+
+// To generate Japanese invoices:
+// Nihon no seikyū-sho o seisei suru ni wa:
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+// User command to run an application
+Console.WriteLine(@"c:\invoices\app.exe -j");
