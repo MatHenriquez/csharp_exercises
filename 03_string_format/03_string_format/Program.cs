548860a889fa5111693a3efec1a45ae6bbbddef8
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-03 11:41:47 -0300

    challenge complete

--- 03_string_format/03_string_format/Program.cs
+++ 03_string_format/03_string_format/Program.cs
@@ -56,5 +56,29 @@ Console.WriteLine(consoleMessage);
 */
 
 // Combinación de los literales textuales y la interpolación de cadenas
+
 string projectName = "First-Project";
 Console.Write($@"C:\Output\{projectName}\Data");
+
+// Desafío
+
+/*
+ * Lograr el siguiente Output:
+ * View English output:
+ *     c:\Exercise\ACME\data.txt
+ *
+ * Посмотреть русский вывод:
+ *     c:\Exercise\ACME\ru-RU\data.txt
+ *     
+ *     Utilizando las variables dadas.
+*/
+
+//Resolución:
+string myProjectName = "ACME";
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+Console.WriteLine($@"View English output:
+    c:\Exercise\{myProjectName}\data.txt");
+
+Console.WriteLine($"\n{russianMessage}:\n\tc:\\Exercise\\{myProjectName}\\ru-RU\\data.txt");
