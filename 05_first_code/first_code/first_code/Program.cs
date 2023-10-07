6606a7e83b06fdac368a191370261f1379ccc2b3
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-07 20:59:25 -0300

    exercise statement

--- 05_first_code/first_code/first_code/Program.cs
+++ 05_first_code/first_code/first_code/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿// Ejercicio: calcular el promedio de los alumnos.
+
+// initialize variables - graded assignments 
+int currentAssignments = 5;
+
+int sophia1 = 93;
+int sophia2 = 87;
+int sophia3 = 98;
+int sophia4 = 95;
+int sophia5 = 100;
+
+int nicolas1 = 80;
+int nicolas2 = 83;
+int nicolas3 = 82;
+int nicolas4 = 88;
+int nicolas5 = 85;
+
+int zahirah1 = 84;
+int zahirah2 = 96;
+int zahirah3 = 73;
+int zahirah4 = 85;
+int zahirah5 = 79;
+
+int jeong1 = 90;
+int jeong2 = 92;
+int jeong3 = 98;
+int jeong4 = 100;
+int jeong5 = 97;
