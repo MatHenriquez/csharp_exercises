df05e23418e42ecca9977cd99578fea2f8378709
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-07 21:05:12 -0300

    initializing variables

--- 05_first_code/first_code/first_code/Program.cs
+++ 05_first_code/first_code/first_code/Program.cs
@@ -26,3 +26,13 @@ int jeong2 = 92;
 int jeong3 = 98;
 int jeong4 = 100;
 int jeong5 = 97;
+
+int sophiaSum = 0;
+int nicolasSum = 0;
+int zahirahSum = 0;
+int jeongSum = 0;
+
+Console.WriteLine("Sophia: " + sophiaSum);
+Console.WriteLine("Nicolas: " + nicolasSum);
+Console.WriteLine("Zahirah: " + zahirahSum);
+Console.WriteLine("Jeong: " + jeongSum);
