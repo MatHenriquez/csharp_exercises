1a332001ef0997b75b8d9ae5b4dde45848b57d62
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-09 19:22:58 -0300

    done

--- 06_guided_project_final_GPA/finalGPA/finalGPA/Program.cs
+++ 06_guided_project_final_GPA/finalGPA/finalGPA/Program.cs
@@ -47,6 +47,6 @@ Console.WriteLine("Couse\t\t\t\tGrade\tCredit Hours");
 Console.WriteLine($"{course1Name}\t\t\t{course1Grade}\t\t{course1Credit}");
 Console.WriteLine($"{course2Name}\t\t\t{course2Grade}\t\t{course2Credit}");
 Console.WriteLine($"{course3Name}\t\t\t{course3Grade}\t\t{course3Credit}");
-Console.WriteLine($"{course4Name}\t{course4Grade}\t\t{course4Credit}");
-Console.WriteLine($"{course5Name}\t\t{course5Grade}\t\t{course5Credit}");
+Console.WriteLine($"{course4Name}\t\t{course4Grade}\t\t{course4Credit}");
+Console.WriteLine($"{course5Name}\t\t\t{course5Grade}\t\t{course5Credit}");
 Console.WriteLine($"\nFinal GPA:\t\t\t{leadingDigit}.{trailingDigits}");
