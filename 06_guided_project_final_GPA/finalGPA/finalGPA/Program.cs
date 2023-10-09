df0ea6082025c7da4e55f22be120c79f29ccfe21
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-09 19:21:43 -0300

    almost done

--- 06_guided_project_final_GPA/finalGPA/finalGPA/Program.cs
+++ 06_guided_project_final_GPA/finalGPA/finalGPA/Program.cs
@@ -40,10 +40,13 @@ decimal gradePointAverage = (decimal)totalGradePoints / totalCreditHours;
 int leadingDigit = (int)gradePointAverage; // Obtengo la parte entera.
 int firstDigit = (int)(gradePointAverage * 10) % 10; // Obtengo el primer decimal.
 int secondDigit = (int)(gradePointAverage * 100) % 10; // Obtengo el segundo decimal.
+int trailingDigits = (int)(gradePointAverage * 100) - (leadingDigit * 100);
 
-Console.WriteLine($"{course1Name} {course1Grade} {course1Credit}");
-Console.WriteLine($"{course2Name} {course2Grade} {course2Credit}");
-Console.WriteLine($"{course3Name} {course3Grade} {course3Credit}");
-Console.WriteLine($"{course4Name} {course4Grade} {course4Credit}");
-Console.WriteLine($"{course5Name} {course5Grade} {course5Credit}");
-Console.WriteLine($"Final GPA: {leadingDigit}.{firstDigit}{secondDigit}");
+Console.WriteLine($"Student: {studentName}\n");
+Console.WriteLine("Couse\t\t\t\tGrade\tCredit Hours");
+Console.WriteLine($"{course1Name}\t\t\t{course1Grade}\t\t{course1Credit}");
+Console.WriteLine($"{course2Name}\t\t\t{course2Grade}\t\t{course2Credit}");
+Console.WriteLine($"{course3Name}\t\t\t{course3Grade}\t\t{course3Credit}");
+Console.WriteLine($"{course4Name}\t{course4Grade}\t\t{course4Credit}");
+Console.WriteLine($"{course5Name}\t\t{course5Grade}\t\t{course5Credit}");
+Console.WriteLine($"\nFinal GPA:\t\t\t{leadingDigit}.{trailingDigits}");
