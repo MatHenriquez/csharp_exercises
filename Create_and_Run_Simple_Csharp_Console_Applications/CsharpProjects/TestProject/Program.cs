ef4ad7dcb0ef7c462971332882b90336e6ca523e
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-10 11:32:04 -0300

    done

--- Create_and_Run_Simple_Csharp_Console_Applications/CsharpProjects/TestProject/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/CsharpProjects/TestProject/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿//? dotnet new console -o ./CsharpProjects/TestProject
+//* La estructura de un comando de la CLI consta de las tres partes siguientes:
+//*     El controlador: dotnet en este ejemplo.
+//*     El comando: new console en este ejemplo.
+//*     Los argumentos de comando: -o ./CsharpProjects/TestProject en este ejemplo.
+
+
+Console.WriteLine("Hello C#!"); //! Hello C#!
+
+//? dotnet build -------> Crea el proyecto y sus dependencias.
+//? dotnet run -------> Ejecuta el código fuente sin comandos explícitos de compilación o inicio.
