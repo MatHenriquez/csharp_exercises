84d717355c35723d16acd78cff8710d3f39159e1
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-11 19:41:57 -0300

    random library

--- Create_and_Run_Simple_Csharp_Console_Applications/02_library_methods/dotnet_library/dotnet_library/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/02_library_methods/dotnet_library/dotnet_library/Program.cs
@@ -1,1 +1,3 @@
-﻿
+﻿Random dice = new Random(); // Generador de números aleatorios.
+int roll = dice.Next(1, 7); // Obtengo un número del 1 al 6.
+Console.WriteLine(roll);    // Muestro el número obtenido.
