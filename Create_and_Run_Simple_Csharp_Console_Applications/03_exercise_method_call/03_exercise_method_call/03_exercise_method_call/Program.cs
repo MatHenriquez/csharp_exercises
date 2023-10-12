646f7afc85652502d6403cdf02f88934ec45161c
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-12 12:01:13 -0300

    done+

--- Create_and_Run_Simple_Csharp_Console_Applications/03_exercise_method_call/03_exercise_method_call/03_exercise_method_call/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/03_exercise_method_call/03_exercise_method_call/03_exercise_method_call/Program.cs
@@ -1,1 +1,9 @@
 ﻿// Implemente un método de la clase Math que devuelva el mayor de dos números.
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
