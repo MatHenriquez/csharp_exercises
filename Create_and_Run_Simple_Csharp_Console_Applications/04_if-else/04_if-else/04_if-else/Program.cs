d9661ddbfceb4ea6b82876dfedb98d84b2189ec1
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-12 19:07:17 -0300

    exercise statement

--- Create_and_Run_Simple_Csharp_Console_Applications/04_if-else/04_if-else/04_if-else/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/04_if-else/04_if-else/04_if-else/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿/*
+ * Usará el método Random.Next() para simular el lanzamiento de tres dados de seis caras. Para calcular la puntuación, evaluará los valores obtenidos. Si la puntuación es mayor que un total arbitrario, se mostrará un mensaje
+ * de éxito al usuario. Si la puntuación está por debajo del límite, se mostrará un mensaje de fracaso al usuario.
+ * Si dos dados cualesquiera muestran el mismo valor, se obtienen dos puntos extra por obtener un doble.
+ * Si los tres dados muestran el mismo valor, se obtienen seis puntos extra por obtener un triple.
+ * Si la suma de los tres dados, junto con los puntos extra, es igual o mayor que 15, gana el juego. De lo contrario, pierde.
+ */
