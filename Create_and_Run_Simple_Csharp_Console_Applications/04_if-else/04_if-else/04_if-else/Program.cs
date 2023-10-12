6316c668a2d5f90279bd731124e934029eaefa1e
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-12 19:09:52 -0300

    variables

--- Create_and_Run_Simple_Csharp_Console_Applications/04_if-else/04_if-else/04_if-else/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/04_if-else/04_if-else/04_if-else/Program.cs
@@ -5,3 +5,13 @@
  * Si los tres dados muestran el mismo valor, se obtienen seis puntos extra por obtener un triple.
  * Si la suma de los tres dados, junto con los puntos extra, es igual o mayor que 15, gana el juego. De lo contrario, pierde.
  */
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1  + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
