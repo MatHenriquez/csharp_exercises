46df336dcc60b844f7fd3f6fe15da7bb4269fd3a
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-12 19:24:43 -0300

    booleans examples

--- Create_and_Run_Simple_Csharp_Console_Applications/04_if-else/04_if-else/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/04_if-else/04_if-else/Program.cs
@@ -25,3 +25,15 @@ if(total < 15)
 {
     Console.WriteLine("Sorry, you lose.");
 }
+
+// Expresiones booleanas
+
+string message = "The quick brown fox jumps over the lazy dog.";
+bool result = message.Contains("dog");
+
+Console.WriteLine(result);
+
+if(message.Contains("fox"))
+{
+    Console.WriteLine("What does the fox say?");
+}
