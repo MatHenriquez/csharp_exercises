e956016bf8598395fcaf175f94b5fc9f77436bfd
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-12 10:57:57 -0300

    more theory

--- Create_and_Run_Simple_Csharp_Console_Applications/02_library_methods/dotnet_library/dotnet_library/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/02_library_methods/dotnet_library/dotnet_library/Program.cs
@@ -10,3 +10,11 @@ Console.WriteLine(roll);    // Muestro el número obtenido.
 // los métodos con estado se compilan de forma que dependen de los valores almacenados en memoria por líneas anteriores de código que ya se han ejecutado. O bien modifican el estado de la aplicación actualizando valores o almacenando nuevos valores en la memoria. También se conocen como métodos de instancia.
 
 // Una sola clase puede admitir métodos con y sin estado. Sin embargo, cuando necesite llamar a métodos con estado, debe crear primero una instancia de la clase para que el método pueda tener acceso al estado.
+
+// Valores devueltos
+
+// Algunos métodos no devuelven valores cuando finalizan. Se hace referencia a ellos como métodos nulos (void).
+// Otros métodos están diseñados para devolver un valor al finalizar.
+
+// Aunque un método devuelve un valor, podemos llamarlo sin usar el valor devuelto.
+Console.WriteLine(dice.Next(1, 7));
