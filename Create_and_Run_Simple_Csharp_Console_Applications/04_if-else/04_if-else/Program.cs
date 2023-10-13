3716c151f7d91bbedb3bcdaeff37518cbe87b05b
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-13 15:34:08 -0300

    more statements

--- Create_and_Run_Simple_Csharp_Console_Applications/04_if-else/04_if-else/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/04_if-else/04_if-else/Program.cs
@@ -3,7 +3,10 @@
  * de éxito al usuario. Si la puntuación está por debajo del límite, se mostrará un mensaje de fracaso al usuario.
  * Si dos dados cualesquiera muestran el mismo valor, se obtienen dos puntos extra por obtener un doble.
  * Si los tres dados muestran el mismo valor, se obtienen seis puntos extra por obtener un triple.
- * Si la suma de los tres dados, junto con los puntos extra, es igual o mayor que 15, gana el juego. De lo contrario, pierde.
+ * Si el jugador obtiene una puntuación igual o mayor que 16, gana un coche nuevo.
+ * Si el jugador obtiene una puntuación igual o mayor que 10, gana un portátil nuevo.
+ * Si el jugador obtiene una puntuación de 7, gana un viaje.
+ * De lo contrario, el jugador gana un gatito.
  */
 
 Random dice = new Random();
