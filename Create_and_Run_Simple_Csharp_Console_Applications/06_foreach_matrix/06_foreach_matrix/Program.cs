b6f479644b810bac1d5e7c76136077ec9dfedbef
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-13 19:24:37 -0300

    matrix variable declaration

--- Create_and_Run_Simple_Csharp_Console_Applications/06_foreach_matrix/06_foreach_matrix/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/06_foreach_matrix/06_foreach_matrix/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿// Declaración de una nueva matriz
+string[] fraudulentOrderIDs = new string[3]; 
+// El operador new crea una nueva instancia de una matriz en la memoria del equipo que puede contener tres valores de cadena.
+// El primer conjunto de corchetes [] simplemente indica al compilador que la variable denominada fraudulentOrderIDs será una matriz.
+// El segundo conjunto de corchetes [3] indica el número de elementos que puede contener la matriz.
+// El segundo conjunto de corchetes [3] indica el número de elementos que puede contener la matriz.
