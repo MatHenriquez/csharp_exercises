c04cf0c44a9fc2c016f536d4c90d0d38762e5f91
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-13 19:26:21 -0300

    matrix initialized

--- Create_and_Run_Simple_Csharp_Console_Applications/06_foreach_matrix/06_foreach_matrix/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/06_foreach_matrix/06_foreach_matrix/Program.cs
@@ -1,6 +1,10 @@
 ﻿// Declaración de una nueva matriz
-string[] fraudulentOrderIDs = new string[3]; 
+string[] fraudulentOrderIDs = new string[3];
 // El operador new crea una nueva instancia de una matriz en la memoria del equipo que puede contener tres valores de cadena.
 // El primer conjunto de corchetes [] simplemente indica al compilador que la variable denominada fraudulentOrderIDs será una matriz.
 // El segundo conjunto de corchetes [3] indica el número de elementos que puede contener la matriz.
 // El segundo conjunto de corchetes [3] indica el número de elementos que puede contener la matriz.
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
