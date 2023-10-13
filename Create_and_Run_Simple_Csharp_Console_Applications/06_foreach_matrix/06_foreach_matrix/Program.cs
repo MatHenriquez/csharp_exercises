d9b9fa750c47daa0525f362b23a58f8d38a9b100
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-13 19:33:48 -0300

    initialize a matrix

--- Create_and_Run_Simple_Csharp_Console_Applications/06_foreach_matrix/06_foreach_matrix/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/06_foreach_matrix/06_foreach_matrix/Program.cs
@@ -1,13 +1,16 @@
 ﻿// Declaración de una nueva matriz
-string[] fraudulentOrderIDs = new string[3];
+//string[] fraudulentOrderIDs = new string[3];
 // El operador new crea una nueva instancia de una matriz en la memoria del equipo que puede contener tres valores de cadena.
 // El primer conjunto de corchetes [] simplemente indica al compilador que la variable denominada fraudulentOrderIDs será una matriz.
 // El segundo conjunto de corchetes [3] indica el número de elementos que puede contener la matriz.
 // El segundo conjunto de corchetes [3] indica el número de elementos que puede contener la matriz.
 
-fraudulentOrderIDs[0] = "A123";
-fraudulentOrderIDs[1] = "B456";
-fraudulentOrderIDs[2] = "C789";
+//fraudulentOrderIDs[0] = "A123";
+//fraudulentOrderIDs[1] = "B456";
+//fraudulentOrderIDs[2] = "C789";
+
+// Inicialización de una matriz
+string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
 
 Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
 Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
@@ -15,3 +18,5 @@ Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
 
 fraudulentOrderIDs[0] = "F000";
 Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+
