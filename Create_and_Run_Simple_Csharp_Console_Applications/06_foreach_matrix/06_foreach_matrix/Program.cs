f1c6dff0457abbd9badfc48809cbd70474e1d8c6
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-14 19:37:05 -0300

    foreach inventory exercise done

--- Create_and_Run_Simple_Csharp_Console_Applications/06_foreach_matrix/06_foreach_matrix/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/06_foreach_matrix/06_foreach_matrix/Program.cs
@@ -33,3 +33,13 @@ foreach(string name in names)
 {
     Console.WriteLine(name);
 }
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+
+foreach(int item in inventory)
+{
+    sum += item;
+}
+
+Console.WriteLine($"We have {sum} items in inventory.");
