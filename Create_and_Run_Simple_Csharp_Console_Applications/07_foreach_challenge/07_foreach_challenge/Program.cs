727b28266a4b14d960f6c19a8190478274ac69da
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-14 19:51:49 -0300

    foreach challenge statement

--- Create_and_Run_Simple_Csharp_Console_Applications/07_foreach_challenge/07_foreach_challenge/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/07_foreach_challenge/07_foreach_challenge/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * El equipo ha encontrado un patrón. Los pedidos que comienzan por la letra "B" presentan un fraude con una frecuencia 25 veces superior a la normal. 
+ * Deberá escribir otro código que genere el identificador de pedido para los nuevos pedidos que empiecen por la letra "B". 
+ * El equipo antifraude lo usará para investigar más en profundidad.
+ */
+ 
