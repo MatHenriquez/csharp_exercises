41840351bbae0fe8cb838725502454b63afcf2a9
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-14 19:48:48 -0300

    foreach inventory exercise done: bin added

--- Create_and_Run_Simple_Csharp_Console_Applications/06_foreach_matrix/06_foreach_matrix/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/06_foreach_matrix/06_foreach_matrix/Program.cs
@@ -36,10 +36,13 @@ foreach(string name in names)
 
 int[] inventory = { 200, 450, 700, 175, 250 };
 int sum = 0;
+int bin = 0; // Número del contenedor.
 
 foreach(int item in inventory)
 {
     sum += item;
+    bin++;
+    Console.WriteLine($"Bin {bin} items (Running total: {sum})");
 }
 
 Console.WriteLine($"We have {sum} items in inventory.");
