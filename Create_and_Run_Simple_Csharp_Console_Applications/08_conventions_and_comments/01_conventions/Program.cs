ad2d47de502f27246117ccbea8951b60f31fe5a1
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-15 18:39:10 -0300

    correct white spaces usage: three examples so far

--- Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/01_conventions/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/01_conventions/Program.cs
@@ -70,3 +70,63 @@ foreach (string orderID in orderIDs)
 */
 
 // Uso del espacio en blanco para facilitar la legibilidad del código
+
+//El compilador de C# omite los espacios en blanco
+
+//Ejemplo 1:
+//Console
+//.
+//WriteLine
+//(
+//"Hello Example 1!"
+//)
+//;
+
+// Ejemplo 2:
+//string firstWord="Hello";string lastWord="Example 2";Console.WriteLine(firstWord+" "+lastWord+"!");
+
+// Andan pero no son legibles.
+
+// Ejemplo 3:
+
+// Mal
+
+//Random dice = new Random();
+//int roll1 = dice.Next(1, 7);
+//int roll2 = dice.Next(1, 7);
+//int roll3 = dice.Next(1, 7);
+//int total = roll1 + roll2 + roll3;
+//Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+//if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)){
+//    if ((roll1 == roll2) && (roll2 == roll3)){
+//        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+//        total += 6;
+//    } else {
+//        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+//        total += 2;
+//    }
+//}
+
+// Bien:
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+}
