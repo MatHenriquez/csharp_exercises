104d46ce57d8a5a3792fde700290d23aef48d59b
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-15 18:19:38 -0300

    first exercise done

--- Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/01_conventions/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/01_conventions/Program.cs
@@ -31,5 +31,8 @@ bool processedCustomer;
 
 // Ejercicio:
 string firstName = "Bob";
-int widgetsSold = 7;
-Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+int widgetsPurchased = 7;
+/* Testing a change to the message.
+ * int widgetsSold = 7;
+ * Console.WriteLine($"{firstName} sold {widgetsSold} widgets."); */ 
+Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
