1c98a780b713cf0b572f3aed28e113c41293a056
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-15 19:10:33 -0300

    chalenge: resolution

--- Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/02_code_readability_challenge/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/02_code_readability_challenge/Program.cs
@@ -38,3 +38,29 @@ foreach(char character in splitedMessage)
 string newMessage = new string(splitedMessage);
 Console.WriteLine(newMessage);
 Console.WriteLine($"'o' appears {countOfO} times.");
+
+/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+
+    string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+    char[] message = originalMessage.ToCharArray();
+    Array.Reverse(message);
+
+    int letterCount = 0;
+
+    foreach (char letter in message)
+    {
+        if (letter == 'o')
+        {
+            letterCount++;
+        }
+    }
+
+    string newMessage = new String(message);
+
+    Console.WriteLine(newMessage);
+    Console.WriteLine($"'o' appears {letterCount} times.");
+*/
