f04da42a5d3c09c8053976e3fbc0df03e9fa5e24
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-15 18:32:01 -0300

    correct white spaces usage

--- Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/01_conventions/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/01_conventions/Program.cs
@@ -43,19 +43,19 @@ Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
 Random random = new Random();
 string[] orderIDs = new string[5];
 // Loop through each blank orderID
-for( int i = 0; i < orderIDs.Length; i++)
+for (int i = 0; i < orderIDs.Length; i++)
 {
-    // Get a random value that equates to ASCII letters A through E
-    int prefixValue = random.Next(65, 70);
-    // Convert the random value into a char, then a string
-    string prefix = Convert.ToChar(prefixValue).ToString();
-    // Create a random number, pad with zeroes
-    string suffix = random.Next(1, 1000).ToString("000");
-    // Combine the prefix and suffix together, then assign to current orderID
-    orderIDs[i] = prefix + suffix;
+// Get a random value that equates to ASCII letters A through E
+int prefixValue = random.Next(65, 70);
+// Convert the random value into a char, then a string
+string prefix = Convert.ToChar(prefixValue).ToString();
+// Create a random number, pad with zeroes
+string suffix = random.Next(1, 1000).ToString("000");
+// Combine the prefix and suffix together, then assign to current orderID
+orderIDs[i] = prefix + suffix;
 }
 // Print out each orderID
-foreach( string orderID in orderIDs)
+foreach (string orderID in orderIDs)
 {
     Console.WriteLine(orderID);
 }
@@ -68,3 +68,5 @@ foreach( string orderID in orderIDs)
   consist of a letter from A to E, and a three
   digit number. Ex. A123.
 */
+
+// Uso del espacio en blanco para facilitar la legibilidad del código
