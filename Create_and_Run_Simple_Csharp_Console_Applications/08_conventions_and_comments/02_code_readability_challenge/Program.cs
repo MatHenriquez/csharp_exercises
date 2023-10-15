280fcef6cf677e9ca08feceb37080301d67f8c76
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-15 19:08:29 -0300

    chalenge: my attempt

--- Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/02_code_readability_challenge/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/02_code_readability_challenge/Program.cs
@@ -17,3 +17,24 @@
  * Console.WriteLine(new_message);
  * Console.WriteLine($"'o' appears {x} times.");
  */
+
+// This code inverts a string and counts the number of times the character 'o' appears in it.
+
+string message = "The quick brown fox jumps over the lazy dog";
+
+char[] splitedMessage = message.ToCharArray();
+Array.Reverse(splitedMessage);
+
+int countOfO = 0;
+
+foreach(char character in splitedMessage)
+{
+    if(character == 'o')
+    {
+        countOfO++;
+    }
+}
+
+string newMessage = new string(splitedMessage);
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {countOfO} times.");
