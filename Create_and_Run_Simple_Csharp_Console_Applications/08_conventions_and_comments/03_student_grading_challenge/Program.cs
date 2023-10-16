8a945650be53d994b54a768189fb41784bdf9735
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-16 16:04:36 -0300

    challenge: declaring students variables

--- Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/03_student_grading_challenge/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/03_student_grading_challenge/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿// Programa para llevar registro de calificaciones de 5 estudiantes.
+Random calification = new Random();
+
+// Obtengo las calificaciones de cada estudiante de manera aleatoria.
+int[] student1 = { calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101) };
+int[] student2 = { calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101) };
+int[] student3 = { calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101) };
+int[] student4 = { calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101) };
+int[] student5 = { calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101) };
+
+Console.WriteLine(student1[0]);
