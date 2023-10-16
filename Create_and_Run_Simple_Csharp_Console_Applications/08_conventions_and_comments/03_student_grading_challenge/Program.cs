19f2d4e12490a60c9fbbf55faf62f5879821a1fd
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-16 16:13:57 -0300

    challenge: new format for student variables

--- Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/03_student_grading_challenge/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/03_student_grading_challenge/Program.cs
@@ -1,11 +1,16 @@
 ﻿// Programa para llevar registro de calificaciones de 5 estudiantes.
-Random calification = new Random();
+
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" }; // Listado de alumnos.
+
+int examAssignments = 5;
 
 // Obtengo las calificaciones de cada estudiante de manera aleatoria.
-int[] student1 = { calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101) };
-int[] student2 = { calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101) };
-int[] student3 = { calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101) };
-int[] student4 = { calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101) };
-int[] student5 = { calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101) };
+Random calification = new Random();
+int[] sophiaScore = new int[] { calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101) };
+int[] andrewScore = new int[] { calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101) };
+int[] emmaScore = new int[] { calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101) };
+int[] loganScore = new int[] { calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101), calification.Next(0, 101) };
 
-Console.WriteLine(student1[0]);
+// Añado los créditos adicionales de los alumnos.
+
+Console.WriteLine(sophiaScore[2]);
