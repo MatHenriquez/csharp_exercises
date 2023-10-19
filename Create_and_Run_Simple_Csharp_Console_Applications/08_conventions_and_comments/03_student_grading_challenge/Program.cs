7638bd8dc6d4e7c1f561169fa76a0ed63b9abfca
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-18 22:16:58 -0300

    students names printed

--- Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/03_student_grading_challenge/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/03_student_grading_challenge/Program.cs
@@ -25,15 +25,17 @@ int currentAssignments = 5;
 // Student names
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
+foreach(string name in studentNames)
+{
+    Console.WriteLine(name);
+}
+
 int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
 int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
 int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
 int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
 int sophiaSum = 0;
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
 
 decimal sophiaScore;
 
