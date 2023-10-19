f6a2470d7dc1e6cb727c8bd5010bdd6a8ed1be55
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-18 22:21:31 -0300

    moving sophiaScore logic

--- Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/03_student_grading_challenge/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/03_student_grading_challenge/Program.cs
@@ -25,32 +25,29 @@ int currentAssignments = 5;
 // Student names
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
-foreach(string name in studentNames)
-{
-    Console.WriteLine(name);
-}
-
 int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
 int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
 int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
 int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
-int sophiaSum = 0;
-
-decimal sophiaScore;
+foreach(string name in studentNames)
+{
+    int sophiaSum = 0;
+    decimal sophiaScore;
 
 foreach(int score in sophiaScores)
 {
     // add the exam score to the sum
     sophiaSum += score;
 }
+    
+    sophiaSum = sophiaScores[0] + sophiaScores[1] + sophiaScores[2] + sophiaScores[3] + sophiaScores[4];
 
-sophiaSum = sophiaScores[0] + sophiaScores[1] + sophiaScores[2] + sophiaScores[3] + sophiaScores[4];
+    sophiaScore = (decimal)sophiaSum / currentAssignments;
 
-sophiaScore = (decimal)sophiaSum / currentAssignments;
-
-Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+    Console.WriteLine("Student\t\tGrade\n");
+    Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+}
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
