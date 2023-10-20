348fd94f5f06d035f5c4f8638ea3b04aa0c70327
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-19 21:12:41 -0300

    if-elseif-else for letter student grade

--- Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/03_student_grading_challenge/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/03_student_grading_challenge/Program.cs
@@ -25,6 +25,7 @@ int currentAssignments = 5;
 // Student variables
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 int[] studentScores = new int[10];
+string currentStudentLetterGrade = "";
 
 int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
 int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
@@ -60,6 +61,48 @@ foreach (string name in studentNames)
     }
 
     currentStudentGrade = (decimal)sumAssignmentScore / currentAssignments;
+
+    if (currentStudentGrade >= 97 && currentStudentGrade <= 100)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D+";
+
+    else
+        currentStudentLetterGrade = "F";
 
     Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
 }
