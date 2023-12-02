2b0a73a626fa00495918dcd9bc32e74f7e9d8cf1
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-12-02 19:25:09 -0300

    Update code: 02/12 - Ready to go

--- Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/03_student_grading_challenge/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/03_student_grading_challenge/Program.cs
@@ -19,19 +19,22 @@ This C# console application is designed to:
     Logan:          91.2    A-
 */
 
-// initialize variables - graded assignments 
+// initialize variables - graded assignments
 int currentAssignments = 5;
 
-// Student variables
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+
+// Student names
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
 int[] studentScores = new int[10];
+
 string currentStudentLetterGrade = "";
 
-int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
-int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
-int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
-int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
-
+// Display the Report Header
 Console.WriteLine("Student\t\tGrade\n");
 
 foreach (string name in studentNames)
@@ -39,30 +42,36 @@ foreach (string name in studentNames)
     string currentStudent = name;
 
     if (currentStudent == "Sophia")
+        // assign Sophia's scores to the studentScores array 
         studentScores = sophiaScores;
 
     else if (currentStudent == "Andrew")
+        // assign Andrew's scores to the studentScores array 
         studentScores = andrewScores;
 
     else if (currentStudent == "Emma")
+        // assign Emma's scores to the studentScores array 
         studentScores = emmaScores;
 
-    else if(currentStudent == "Logan")
+    else if (currentStudent == "Logan")
+        // assign Logan's scores to the studentScores array 
         studentScores = loganScores;
-    
-    int sumAssignmentScore = 0;
 
+    // initialize/reset the sum of scored assignments
+    int sumAssignmentScores = 0;
+
+    // initialize/reset the calculated average of exam + extra credit scores
     decimal currentStudentGrade = 0;
 
-    foreach(int score in studentScores)
+    foreach (int score in studentScores)
     {
         // add the exam score to the sum
-        sumAssignmentScore += score;
+        sumAssignmentScores += score;
     }
 
-    currentStudentGrade = (decimal)sumAssignmentScore / currentAssignments;
+    currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
 
-    if (currentStudentGrade >= 97 && currentStudentGrade <= 100)
+    if (currentStudentGrade >= 97)
         currentStudentLetterGrade = "A+";
 
     else if (currentStudentGrade >= 93)
@@ -83,14 +92,11 @@ foreach (string name in studentNames)
     else if (currentStudentGrade >= 77)
         currentStudentLetterGrade = "C+";
 
-
     else if (currentStudentGrade >= 73)
         currentStudentLetterGrade = "C";
 
-
     else if (currentStudentGrade >= 70)
         currentStudentLetterGrade = "C-";
-
 
     else if (currentStudentGrade >= 67)
         currentStudentLetterGrade = "D+";
@@ -99,7 +105,7 @@ foreach (string name in studentNames)
         currentStudentLetterGrade = "D";
 
     else if (currentStudentGrade >= 60)
-        currentStudentLetterGrade = "D+";
+        currentStudentLetterGrade = "D-";
 
     else
         currentStudentLetterGrade = "F";
