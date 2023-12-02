41f53f1e5cacc2055bbb28d5d7ef8fee09546833
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-12-02 19:53:46 -0300

    Complete lesson

--- Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/03_student_grading_challenge/Program.cs
+++ Create_and_Run_Simple_Csharp_Console_Applications/08_conventions_and_comments/03_student_grading_challenge/Program.cs
@@ -20,15 +20,19 @@ This C# console application is designed to:
 */
 
 // initialize variables - graded assignments
-int currentAssignments = 5;
+int examAssignments = 5;
 
-int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
-int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
-int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
-int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };
 
 // Student names
-string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
 
 int[] studentScores = new int[10];
 
@@ -57,19 +61,46 @@ foreach (string name in studentNames)
         // assign Logan's scores to the studentScores array 
         studentScores = loganScores;
 
+    else if (currentStudent == "Becky")
+        studentScores = beckyScores;
+
+    else if (currentStudent == "Chris")
+        studentScores = chrisScores;
+
+    else if (currentStudent == "Eric")
+        studentScores = ericScores;
+
+    else if (currentStudent == "Gregor")
+        studentScores = gregorScores;
+
+    else
+        continue;
+
     // initialize/reset the sum of scored assignments
     int sumAssignmentScores = 0;
+
+    // Initialize/reset a counter for the number of assignments.
+    int gradedAssignments = 0;
 
     // initialize/reset the calculated average of exam + extra credit scores
     decimal currentStudentGrade = 0;
 
+    // loop through the scores array and complete calculations for currentStudent
     foreach (int score in studentScores)
     {
-        // add the exam score to the sum
-        sumAssignmentScores += score;
+        // Increment the assignment counter
+        gradedAssignments += 1;
+
+        if (gradedAssignments <= examAssignments)
+            // add the exam score to the sum
+            sumAssignmentScores += score;
+
+        else
+            // Add the extra credit points to the sum - bonus points equal to 10% of an exam score.
+            sumAssignmentScores += score / 10;
     }
 
-    currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
+    currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
 
     if (currentStudentGrade >= 97)
         currentStudentLetterGrade = "A+";
