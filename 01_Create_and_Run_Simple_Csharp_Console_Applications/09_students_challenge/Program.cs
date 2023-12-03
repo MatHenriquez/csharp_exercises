913a65491899e4cbbcd2c79ecc36bdf444aa6d1c
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-12-03 18:39:52 -0300

    Add new project

--- 01_Create_and_Run_Simple_Csharp_Console_Applications/09_students_challenge/Program.cs
+++ 01_Create_and_Run_Simple_Csharp_Console_Applications/09_students_challenge/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Main
+{
+    class Challenge
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
--- 01_Create_and_Run_Simple_Csharp_Console_Applications/09_students_challenge/obj/Debug/net7.0/09_students_challenge.AssemblyInfo.cs
+++ 01_Create_and_Run_Simple_Csharp_Console_Applications/09_students_challenge/obj/Debug/net7.0/09_students_challenge.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("09_students_challenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae244ffd86e1de5cde15d26f195d0a433f9124bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+321958c96781e6b63b506f17deaf674399f87773")]
 [assembly: System.Reflection.AssemblyProductAttribute("09_students_challenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("09_students_challenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
