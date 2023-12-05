b3efd5c5ea7ee1f4394a1773be8f863c6c3b3a81
MatHenriquez <matias.henriquez.dev@gmail.com>
2023-12-04 21:52:38 -0300

    Exemplify inequality operator vs logical negation operator

--- 02_Adding_logic/00_booleans/Program.cs
+++ 02_Adding_logic/00_booleans/Program.cs
@@ -54,3 +54,16 @@ Console.WriteLine(pangram.Contains("fox") == false); // Equals to
 Console.WriteLine(!pangram.Contains("fox"));
 
 Console.WriteLine(!pangram.Contains("cow"));
+
+Console.WriteLine("==================================================");
+
+// Inequality operator vs logical negation operator
+
+int a = 7;
+int b = 6;
+Console.WriteLine(a != b); // output: True
+
+string s1 = "Hello";
+string s2 = "Hello";
+
+Console.WriteLine(s1 != s2); // output: False
--- 02_Adding_logic/00_booleans/obj/Debug/net7.0/00_booleans.AssemblyInfo.cs
+++ 02_Adding_logic/00_booleans/obj/Debug/net7.0/00_booleans.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("00_booleans")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+259512dd4345eb1f01b2b6712980506267e78cec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa961199b3fb4522d2104a2e61b5f87c7f424a8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("00_booleans")]
 [assembly: System.Reflection.AssemblyTitleAttribute("00_booleans")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
