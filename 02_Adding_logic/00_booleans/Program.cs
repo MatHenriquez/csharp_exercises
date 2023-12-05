a9cdbacb5bb954bddf5d9b7eae763d32edd97b66
MatHenriquez <matias.henriquez.dev@gmail.com>
2023-12-04 21:14:14 -0300

    Exemplify the equality operator

--- 02_Adding_logic/00_booleans/Program.cs
+++ 02_Adding_logic/00_booleans/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string myValue = "a";
+Console.WriteLine(myValue == "a");
+
+string value1 = " a";
+string value2 = "A ";
+
+Console.WriteLine(value1 == value2);
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
