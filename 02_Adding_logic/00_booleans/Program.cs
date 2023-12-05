259512dd4345eb1f01b2b6712980506267e78cec
MatHenriquez <matias.henriquez.dev@gmail.com>
2023-12-04 21:22:09 -0300

    Exemplify methods returning booleans

--- 02_Adding_logic/00_booleans/Program.cs
+++ 02_Adding_logic/00_booleans/Program.cs
@@ -41,3 +41,9 @@ Console.WriteLine(1 < 2);
 Console.WriteLine(1 >= 1);
 Console.WriteLine(1 <= 1);
 
+Console.WriteLine("==================================================");
+
+// Methods returning booleans
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
