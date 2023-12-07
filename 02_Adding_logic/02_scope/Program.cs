d47a0fa945c8cef1971d575d700965fef61621a9
MatHenriquez <matias.henriquez.dev@gmail.com>
2023-12-06 21:44:20 -0300

    Exemplify variable scopes

--- 02_Adding_logic/02_scope/Program.cs
+++ 02_Adding_logic/02_scope/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿bool flag = true;
+int value = 0;
+if(flag)
+{
+    value = 10;
+    Console.WriteLine($"Inside of code block: {value}");
+}
+
+Console.WriteLine($"Outside of code block: {value}");
