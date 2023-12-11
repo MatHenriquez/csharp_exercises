f101343621a27f45e511aa4d3d27d06117a82d41
MatHenriquez <matias.henriquez.dev@gmail.com>
2023-12-11 20:28:32 -0300

    Add my solution to the FizzBuzz challenge

--- 02_Adding_logic/04_for/Program.cs
+++ 02_Adding_logic/04_for/Program.cs
@@ -32,3 +32,28 @@ for(int i = 0; i < names.Length; i++)
 
 foreach(string name in names)
     Console.WriteLine(name);
+
+// FizzBuzz challenge
+
+/*
+ * Consigna:
+ * Valores de salida de 1 a 100, un número por línea, dentro del bloque de código de una instrucción de iteración.
+ * Si el valor actual es divisible por 3, se imprime el término Fizz junto al número.
+ * Si el valor actual es divisible por 5, se imprime el término Buzz junto al número.
+ * Si el valor actual es divisible por 3 y por 5, se imprime el término FizzBuzz junto al número.
+ */
+
+for(int i = 1; i <= 100; i++)
+{
+    bool isDivisibleByThree = i % 3 == 0;
+    bool isDivisibleByFive = i % 5 == 0;
+
+    if (isDivisibleByThree && isDivisibleByFive)
+        Console.WriteLine($"{i} - FizzBuzz");
+    else if (isDivisibleByThree)
+        Console.WriteLine($"{i} - Fizz");
+    else if (isDivisibleByFive)
+        Console.WriteLine($"{i} - Buzz");
+    else
+        Console.WriteLine(i);
+}
--- 02_Adding_logic/04_for/obj/Debug/net7.0/04_for.AssemblyInfo.cs
+++ 02_Adding_logic/04_for/obj/Debug/net7.0/04_for.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("04_for")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d75ba7b9b56b511c5ce637e99a4a98aecd44f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62df727d1d98a8f41339ea21f4f8ea3484e4a052")]
 [assembly: System.Reflection.AssemblyProductAttribute("04_for")]
 [assembly: System.Reflection.AssemblyTitleAttribute("04_for")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
