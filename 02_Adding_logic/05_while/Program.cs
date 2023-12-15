f64c8aa29f9bc278420035ec307f185567eda944
MatHenriquez <matias.henriquez.dev@gmail.com>
2023-12-14 21:32:47 -0300

    Exemplify do-while loop

--- 02_Adding_logic/05_while/Program.cs
+++ 02_Adding_logic/05_while/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+int current = 0;
+
+do
+{
+    current = random.Next(1, 11);
+    Console.WriteLine(current);
+}  while (current != 7);
