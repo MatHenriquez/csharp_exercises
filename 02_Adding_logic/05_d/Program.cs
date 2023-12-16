db1aa3fa32f89cd88afe433c8c9b352ad47f2bd6
MatHenriquez <matias.henriquez.dev@gmail.com>
2023-12-15 23:39:11 -0300

    Add challenge statement

--- 02_Adding_logic/05_d/Program.cs
+++ 02_Adding_logic/05_d/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Estas son las condiciones que el primer proyecto de codificación debe implementar:
+
+//La solución debe incluir una iteración do-while o while.
+
+//Antes del bloque de iteración: la solución debe usar una instrucción Console.WriteLine() para solicitar al usuario un valor de enteros entre 5 y 10.
+
+//Dentro del bloque de iteración:
+
+//La solución debe usar una instrucción Console.ReadLine() para obtener la entrada del usuario.
+//La solución debe asegurarse de que la entrada es una representación válida de un entero.
+//Si el valor del entero no está comprendido entre 5 y 10, el código debe usar una instrucción Console.WriteLine() para solicitar al usuario un valor del entero entre 5 y 10.
+//La solución debe garantizar que el valor de enteros está comprendido entre 5 y 10 antes de salir de la iteración.
+//Debajo (después) del bloque de código de iteración, la solución debe usar una instrucción Console.WriteLine() para informar al usuario de que se ha aceptado su valor de entrada.
