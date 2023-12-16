b03ad4beb9893d1950e2ad2e2d2780ddc401c46f
MatHenriquez <matias.henriquez.dev@gmail.com>
2023-12-16 00:04:22 -0300

    Add second challenge statement

--- 02_Adding_logic/05_d/Program.cs
+++ 02_Adding_logic/05_d/Program.cs
@@ -1,4 +1,6 @@
-﻿//Estas son las condiciones que el primer proyecto de codificación debe implementar:
+﻿// Project 1
+
+//Estas son las condiciones que el primer proyecto de codificación debe implementar:
 
 //La solución debe incluir una iteración do-while o while.
 
@@ -34,3 +36,20 @@ do
 } while (!validEntry);
 
 Console.WriteLine("You typed a valid number.");
+
+// Project 2
+
+//Estas son las condiciones que el segundo proyecto de codificación debe implementar:
+
+//La solución debe incluir una iteración do-while o while.
+
+//Antes del bloque de iteración, la solución debe usar una instrucción Console.WriteLine() para solicitar al usuario uno de los tres nombres de rol: Administrador, Director o Usuario.
+
+//Dentro del bloque de iteración:
+
+//La solución debe usar una instrucción Console.ReadLine() para obtener la entrada del usuario.
+//La solución debe garantizar que el valor especificado coincide con una de las tres opciones de rol.
+//La solución debe usar el método Trim() en el valor de entrada para omitir los caracteres de espacio iniciales y finales.
+//La solución debe usar el método ToLower() en el valor de entrada para omitir el caso.
+//Si el valor especificado no coincide con una de las opciones de rol, el código debe usar una instrucción Console.WriteLine() para solicitar al usuario una entrada válida.
+//Debajo (después) del bloque de código de iteración, la solución debe usar una instrucción Console.WriteLine() para informar al usuario de que se ha aceptado su valor de entrada.
