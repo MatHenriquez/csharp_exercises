1a20a9af298087807d7c78e32a20111a2fd9a2e8
MatHenriquez <matias.henriquez.dev@gmail.com>
2023-12-15 23:14:44 -0300

    Add challenge statement

--- 02_Adding_logic/05_b_rol_game/Program.cs
+++ 02_Adding_logic/05_b_rol_game/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Estas son las reglas para el juego de batalla que necesita implementar en el proyecto de código:
+ * Debes usar la instrucción do-while o la instrucción while como un bucle de juego externo.
+ * El héroe y el monstruo comenzarán con 10 puntos de salud.
+ * Todos los ataques serán un valor comprendido entre 1 y 10.
+ * El héroe atacará primero.
+ * Imprima la cantidad de salud que ha perdido el monstruo y su salud restante.
+ * Si la salud del monstruo es mayor que 0, puede atacar al héroe.
+ * Imprima la cantidad de salud que ha perdido el héroe y su salud restante.
+ * Continúe con esta secuencia de ataque hasta que la salud del monstruo o del héroe sea cero o menos.
+ * Imprima el ganador.
+ */
