eab1c104b52ef17749d7fb7c6f9a81c5d2a212ad
MatHenriquez <matias.henriquez.dev@gmail.com>
2023-12-15 23:23:33 -0300

    Add my solution

--- 02_Adding_logic/05_b_rol_game/Program.cs
+++ 02_Adding_logic/05_b_rol_game/Program.cs
@@ -10,3 +10,36 @@
  * Continúe con esta secuencia de ataque hasta que la salud del monstruo o del héroe sea cero o menos.
  * Imprima el ganador.
  */
+
+// My solution
+
+int heroHealth = 10;
+int monsterHealth = 10;
+Random random = new Random();
+
+do
+{
+    int heroAttack = random.Next(1, 11);
+    monsterHealth -= heroAttack;
+
+    Console.WriteLine($"Monster was damaged and lost {heroAttack} health and now has {monsterHealth} health");
+
+    if (monsterHealth <= 0)
+    {
+        Console.WriteLine("Hero wins!");
+        continue;
+    }
+
+    int monsterAttack = random.Next(1, 11); 
+    heroHealth -= monsterAttack;
+
+    Console.WriteLine($"Hero was damaged and lost {monsterAttack} health and now has {heroHealth} health");
+
+    if (heroHealth <= 0)
+    {
+        Console.WriteLine("Monster wins!");
+        continue;
+    }
+} while (heroHealth > 0 && monsterHealth > 0);
+
+Console.WriteLine("Game Over");
