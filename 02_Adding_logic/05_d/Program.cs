2332ec37e64c6e9cae6c8ef0196e01dca6d13fc0
MatHenriquez <matias.henriquez.dev@gmail.com>
2023-12-16 00:02:56 -0300

    Add my solution

--- 02_Adding_logic/05_d/Program.cs
+++ 02_Adding_logic/05_d/Program.cs
@@ -11,3 +11,26 @@
 //Si el valor del entero no está comprendido entre 5 y 10, el código debe usar una instrucción Console.WriteLine() para solicitar al usuario un valor del entero entre 5 y 10.
 //La solución debe garantizar que el valor de enteros está comprendido entre 5 y 10 antes de salir de la iteración.
 //Debajo (después) del bloque de código de iteración, la solución debe usar una instrucción Console.WriteLine() para informar al usuario de que se ha aceptado su valor de entrada.
+
+string? userInput;
+int numericInput = 0;
+bool isInt = false;
+bool validEntry = false;
+
+Console.WriteLine("Type a number between 5 and 10:");
+
+do
+{
+    userInput = Console.ReadLine();
+    isInt = int.TryParse(userInput, out numericInput);
+
+    validEntry = isInt && (numericInput >= 5 && numericInput <= 10);
+
+    if (validEntry)
+        continue;        
+
+    Console.WriteLine("Please, type a number between 5 and 10:");
+
+} while (!validEntry);
+
+Console.WriteLine("You typed a valid number.");
