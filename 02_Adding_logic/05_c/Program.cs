f7a65bd89e94afc497e07e86534c09dc444ae1c0
MatHenriquez <matias.henriquez.dev@gmail.com>
2023-12-15 23:32:54 -0300

    Add an example of while vs do-while

--- 02_Adding_logic/05_c/Program.cs
+++ 02_Adding_logic/05_c/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? readResult;
+Console.WriteLine("Enter a string:");
+do
+{
+    readResult = Console.ReadLine();
+} while (readResult == null);
