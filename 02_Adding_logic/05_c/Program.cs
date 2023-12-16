bb9f902479a9d64033028ce3339d6366e45965b3
MatHenriquez <matias.henriquez.dev@gmail.com>
2023-12-15 23:35:47 -0300

    Add a second example of while vs do-while

--- 02_Adding_logic/05_c/Program.cs
+++ 02_Adding_logic/05_c/Program.cs
@@ -4,3 +4,21 @@ do
 {
     readResult = Console.ReadLine();
 } while (readResult == null);
+
+bool validEntry = false;
+Console.WriteLine("Enter a string containing at least three characters:");
+do
+{
+    readResult = Console.ReadLine();
+    if (readResult != null)
+    {
+        if(readResult.Length >= 3) 
+        {
+            validEntry = true;
+        }
+        else
+        {
+            Console.WriteLine("Your input is invalid, please try again.");
+        }
+    }
+} while (validEntry == false);
