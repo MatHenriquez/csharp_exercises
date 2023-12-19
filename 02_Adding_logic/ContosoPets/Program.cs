e6c66562af2bdedfad6158188266c297c67d17e6
MatHenriquez <matias.henriquez.dev@gmail.com>
2023-12-19 20:43:36 -0300

    Declare initial variables

--- 02_Adding_logic/ContosoPets/Program.cs
+++ 02_Adding_logic/ContosoPets/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string animalSpecies = "";
+string animalID = "";
+string animalAge = "";
+string animalPhysicalDescription = "";
+string animalPersonalityDescription = "";
+string animalNickname = "";
+
+int maxPets = 8;
+string? readResult;
+string menuSelection = "";
+
+string[,] ourAnimals = new string[maxPets, 6];
+
