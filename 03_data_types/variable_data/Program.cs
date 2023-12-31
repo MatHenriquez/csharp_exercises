f503daf669b21424bde12de42fef0fb72b7d96c6
MatHenriquez <matias.henriquez.dev@gmail.com>
2023-12-30 22:42:41 -0300

    Display max and mins of float, double and decimal types

--- 03_data_types/variable_data/Program.cs
+++ 03_data_types/variable_data/Program.cs
@@ -18,5 +18,11 @@ public class Program
         Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
         Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
         Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+        Console.WriteLine("");
+        Console.WriteLine("Floating point types:");
+        Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+        Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+        Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
     }
 }
--- 03_data_types/variable_data/obj/Debug/net7.0/variable_data.AssemblyInfo.cs
+++ 03_data_types/variable_data/obj/Debug/net7.0/variable_data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("variable_data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1fa5716b0d5860b7a1080af2dbc31a0410945ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a037aa232444ef34e19344eb1495490ba023593")]
 [assembly: System.Reflection.AssemblyProductAttribute("variable_data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("variable_data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
