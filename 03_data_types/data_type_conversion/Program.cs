3ce2024a6aeaf4319576e639c4684b0bf6058a47
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-01-07 19:19:46 -0300

    Exemplify ampliation conversion

--- 03_data_types/data_type_conversion/Program.cs
+++ 03_data_types/data_type_conversion/Program.cs
@@ -3,3 +3,9 @@ string second = "4";
 //int result = first + second; // Error
 string result = first + second; // 24
 Console.WriteLine(result);
+
+int myInt = 3;
+Console.WriteLine($"int: {myInt}");
+
+decimal myDecimal = myInt; // It will not lose information
+Console.WriteLine($"decimal: {myDecimal}");
