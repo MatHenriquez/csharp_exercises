83b3f7e40e435fcea91ac1b68b545f1761abc8c7
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-01-07 21:30:17 -0300

    Add second challenge statement

--- 03_data_types/data_type_conversion/Program.cs
+++ 03_data_types/data_type_conversion/Program.cs
@@ -60,7 +60,7 @@ else
 if (myResult > 0)
     Console.WriteLine($"Measurement (w/ offset): {50 + myResult}");
 
-Console.WriteLine("---- CHALLENGE ----");
+Console.WriteLine("---- CHALLENGE 1 ----");
 
 string[] values = { "12.3", "45", "ABC", "11", "DEF" };
 
@@ -79,3 +79,19 @@ foreach (string myValue in values)
 
 Console.WriteLine($"Message: {myMessage}");
 Console.WriteLine($"Total: {total}");
+
+Console.WriteLine("---- CHALLENGE 2 ----");
+
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// Your code here to set result1
+// Hint: You need to round the result to nearest integer (don't just truncate)
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+// Your code here to set result2
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// Your code here to set result3
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
