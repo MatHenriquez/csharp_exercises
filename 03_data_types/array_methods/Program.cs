3cb6441c5f8038b573b8d3649114c2afe72d3c33
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-01-08 21:15:46 -0300

    Exemplify Array.Sort()

--- 03_data_types/array_methods/Program.cs
+++ 03_data_types/array_methods/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] pallets = { "B14", "A11", "B12", "A13" };
+
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
