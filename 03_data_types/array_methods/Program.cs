6bb6d211daed091fedef287c577ec285ac710c79
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-01-09 20:58:57 -0300

    Add second exercise statement

--- 03_data_types/array_methods/Program.cs
+++ 03_data_types/array_methods/Program.cs
@@ -111,3 +111,18 @@ for (int i = 0; i < message.Length; i++)
 //Step 4
 result = String.Join(" ", newMessage);
 Console.WriteLine(result);
+
+// Second challenge
+Console.WriteLine("");
+Console.WriteLine("---- SECOND CHALLENGE ----");
+
+// Expected output:
+//A345
+//B123
+//B177
+//B179
+//C15     - Error
+//C234
+//C235
+//G3003   - Error
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
