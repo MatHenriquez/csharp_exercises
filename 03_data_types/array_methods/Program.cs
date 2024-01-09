52b00ad0644b9333b08d7dd90174a517a1391f36
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-01-08 21:22:33 -0300

    Exemplify empty string vs null

--- 03_data_types/array_methods/Program.cs
+++ 03_data_types/array_methods/Program.cs
@@ -16,8 +16,11 @@ foreach (var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
 }
+Console.WriteLine("");
 
+Console.WriteLine($"Before: {pallets[0]}");
 Array.Clear(pallets, 0, 2);
+Console.WriteLine($"After: {pallets[0]}");
 Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
 foreach (var pallet in pallets)
 {
