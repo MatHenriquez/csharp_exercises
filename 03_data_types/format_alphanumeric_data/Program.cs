9d81895477df1ce20488b4fe1a88982ab2d05192
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-01-11 20:29:18 -0300

    Add a second example of composite format

--- 03_data_types/format_alphanumeric_data/Program.cs
+++ 03_data_types/format_alphanumeric_data/Program.cs
@@ -7,6 +7,7 @@ class Program
         string firstWord = "Hello";
         string secondWord = "World";
         string message = string.Format("{0} {1}!", firstWord, secondWord);
-        Console.WriteLine(message);
+        Console.WriteLine(message); // Hello World!
+        Console.WriteLine("{0} {0} {0}!", firstWord, secondWord); // Hello Hello Hello!
     }
 }
--- 03_data_types/format_alphanumeric_data/obj/Debug/net7.0/format_alphanumeric_data.AssemblyInfo.cs
+++ 03_data_types/format_alphanumeric_data/obj/Debug/net7.0/format_alphanumeric_data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("format_alphanumeric_data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c94d1fd24c5149f4becedbdb1d363a7e826ffac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4963e4a3fde6bff1652a2aa2a08ce401ce2329c")]
 [assembly: System.Reflection.AssemblyProductAttribute("format_alphanumeric_data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("format_alphanumeric_data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
