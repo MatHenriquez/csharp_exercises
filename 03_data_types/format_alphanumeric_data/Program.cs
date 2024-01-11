9c94d1fd24c5149f4becedbdb1d363a7e826ffac
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-01-11 20:25:40 -0300

    Add template

--- 03_data_types/format_alphanumeric_data/Program.cs
+++ 03_data_types/format_alphanumeric_data/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Alphanumeric_Data;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+    }
+}
